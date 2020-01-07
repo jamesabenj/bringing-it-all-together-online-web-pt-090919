23fb71ce8397438d0521ff705370d65bc9d28c18
James Benjamin <james.abenj@gmail.com>
2020-01-07 19:12:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,10 +56,7 @@ def self.find_or_create_by
 end
 
 def self.new_from_db(row)
-  new_dog = self.new
-  new_dog.id = row[0]
-  new_dog.name = row[1]
-  new_dog.breed = row[2]
+  new_dog = self.new(id: row[0], name: row[1], breed: row[2])
   new_dog
 end
 
