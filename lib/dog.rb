9b38ff5edd4a167a02efcc474c1e8e0bddddfe00
James Benjamin <james.abenj@gmail.com>
2020-01-07 19:23:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -75,7 +75,7 @@ def self.find_by_name(name)
   Dog.new(id: result[0], name: result[1], breed: result[2])
 end
 
-def update(id:, name:, breed:)
+def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
 end
